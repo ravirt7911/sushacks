6b1c2bdea4565b43c21c62313f43bce6de1a9cb2
sriramadari <lakshmisriramadari1427@gmail.com>
2024-03-14 21:11:44 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import "./App.css";
 
 function App() {
 
-  return <div className="App">Testing sus hackkkkkkkkkkkkk</div>;
-
-  return <div className="App">SUS HACKS</div>;
-
   return <div className="App"> sus hackkkkkkkkkkkkk</div>;
 }
 
