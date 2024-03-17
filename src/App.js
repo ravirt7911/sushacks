1103e6d06cff33c7396458621da01bec94c74310
Kamsu Ravi Teeja <94612127+ravirt7911@users.noreply.github.com>
2024-03-17 19:33:27 +0530

    Revert "Created About Component (#15)"
    This reverts commit 8ceaf655c4c48ce6487e639a90ce39cc5d3edd24.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import About from "./Components/About/About";
-
-function App() {
-    return (
-        <div className="App">
-            Testing sus hackkkkkkkkkkkkk
-            <About />
-        </div>
-    );
-=======
 import Sponsors from "./components/Sponsors/Sponsors.js";
 import Contact from "./components/Contact Us/Contactus.js";
 import Prizes from "./components/Prizes/Prizes.js";
@@ -30,7 +19,6 @@ function App() {
       <Contact />
     </div>
   );
->>>>>>> 51b670e4b01eab2421ba2fe15f801936547dbb4b
 }
 
 export default App;
