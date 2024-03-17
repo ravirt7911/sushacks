37f6179d8a3219a31253f91b3580a62066fae2e7
Yamani <97576817+Yamani246@users.noreply.github.com>
2024-03-17 10:54:03 +0530

    updated (#7)
    Co-authored-by: Kamsu Ravi Teeja <94612127+ravirt7911@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-
+import Navbar from "./components/nav.jsx";
 function App() {
+  return (
+  <div className="App">
+    < Navbar/>
+    </div>
+  )
 
   return <div className="App">Testing sus hackkkkkkkkkkkkk</div>;
+
 
 }
 
