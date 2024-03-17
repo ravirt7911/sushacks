0b43e5127cc8d4a17a487619e3b871998af6327c
Giribabi <108036188+Giribabi@users.noreply.github.com>
2024-03-17 21:34:11 +0530

    Updated all Components (#18)
    * Created About Component
    
    * Update the App component and packages
    
    * Updated all components
    
    * Update App.js
    
    Updated components' routes
    
    * Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./App.css";
-import Sponsors from "./components/Sponsors/Sponsors.js";
-import Contact from "./components/Contact Us/Contactus.js";
-import Prizes from "./components/Prizes/Prizes.js";
-import Tracks from "./components/Tracks/Tracks.js";
-
-import Navbar from "./components/Navbar/Navbar.js";
-// import Timeline from "./components/Timelines/Timeline.js";
+import About from "./Components/About/About";
+import Sponsors from "./Components/Sponsors/Sponsors.js";
+import Contact from "./Components/Contact Us/Contactus.js";
+import Prizes from "./Components/Prizes/Prizes.js";
+import Navbar from "./Components/Navbar/Navbar.js";
+import Tracks from "./Components/Tracks/Tracks.js";
+//import Timeline from "./components/Timelines/Timeline.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Tracks />
-      {/* <Timeline /> */}
-      <Prizes />
-      <Sponsors />
-      <Contact />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <About />
+            <Tracks />
+            {/* <Timeline /> */}
+            <Prizes />
+            <Sponsors />
+            <Contact />
+        </div>
+    );
 }
 
 export default App;
