e920d3b518e334c68f943442ea091d930bc394ea
uday1508 <udayg256@gmail.com>
2024-03-17 19:47:59 +0530

    integrated

--- src/components/Timelines/Timeline.js
+++ src/components/Timelines/Timeline.js
@@ -1,133 +1,177 @@
-import React, { useState } from "react";
-import "./Timeline.css";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useState } from 'react'
+import './Timeline.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import ProgressBar from "@ramonak/react-progress-bar";
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
+import CardHeader from '@mui/material/CardHeader'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
 const Timeline = () => {
-  const [active, setActive] = useState(0);
+  const [active,setActive] = useState(0);
   return (
-    <div>
-      <div className="card__data">
-        <h3 className="card__title">TIMELINES</h3>
-      </div>
+   <div className='parentc' style={{backgroundImage:`url(${require('./assets/back.png')})`}}
+   >
+   <div className="card__data">
+         <h1 className="card__title" style={{ fontFamily: "'Among Us'" ,transform: 'scaleY(1.5)',fontStretch:'expanded'}}>TIMELINES</h1>                  
+    </div>
 
-      <div className="progresBar">
-        <ProgressBar
-          borderRadius="0"
-          animateOnRender={true}
-          completed={((active + 1) * 100) / 6}
-          labelColor="green"
-          bgColor="green"
-        />
-      </div>
+    <div className="progresBar">
+    <ProgressBar  borderRadius='0' animateOnRender = {true}completed={((active+1)*100)/6}  labelColor='green' bgColor='green'/>
+    </div>
 
+    <div  className='PP' style={{backgroundImage:`url(${require('./assets/phn.jpg')})`,backgroundSize:'cover',backgroundPosition:'center',borderRadius:'20px'}} >
+    
       <Swiper
-        freeMode={true}
-        grabCursor={true}
-        slidesPerView={3}
-        spaceBetween={10}
-        centeredSlides={true}
-        className="card__container"
-        onSlideChange={(e) => setActive(e.activeIndex)}
-        onActiveIndexChange={(e) => console.log(e)}
-        //style={{backgroundImage:`url(${require('./assets/i')})`}}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        autoFocus={true}
+        grabCursor = {true}
+        slidesPerView={1}
+        spaceBetween={40}
+        centeredSlides = {true}
+        // style={{backgroundImage:`url(${require('./assets/phn.jpg')})`,objectFit:'contain'}} 
+        className='card__container'
+        onSlideChange={(e)=>setActive(e.activeIndex)}
+        onActiveIndexChange={(e)=>console.log(e)}
+        pagination = {{
+          el: "swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation = {{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        breakpoints = {{
+          600: {
+            slidesPerView: 2,
+          },
+          968: {
+            slidesPerView: 3,
+          },
+        }}
       >
-        <div className="card__content">
-          <SwiperSlide className="card__article">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_3.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
+
+
+
+  
+
+ 
+
+        
+        <div className='card__content' >
+            
+        <SwiperSlide className='card__article'>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_2.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
             </div>
 
-            <div className="card__data">
-              <h3 className="card__name">Registration Begins</h3>
-              <p className="card__description">20-04-2024</p>
-            </div>
-          </SwiperSlide>
+                <div className="card__data">
+                        <h3 className="card__name">Registration Begins</h3>
+                        <p className="card__description">
+                           20-04-2024
+                        </p>
+                 </div>
+       </SwiperSlide>
 
-          <SwiperSlide className="card__article">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_3.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
-            </div>
-
-            <div className="card__data">
-              <h3 className="card__name">Registration Ends</h3>
-              <p className="card__description">27-04-2024</p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className="card__article ">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_3.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
+       <SwiperSlide className='card__article'>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_3.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
             </div>
 
-            <div className="card__data">
-              <h3 className="card__name">Project Submission</h3>
-              <p className="card__description">29-04-2024</p>
-            </div>
-          </SwiperSlide>
+                <div className="card__data">
+                        <h3 className="card__name">Registration Ends</h3>
+                        <p className="card__description">
+                           27-04-2024
+                        </p>
+                 </div>
 
-          <SwiperSlide className="card__article">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_2.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
-            </div>
+                
+            
+        </SwiperSlide>
 
-            <div className="card__data">
-              <h3 className="card__name">Registration Begins</h3>
-              <p className="card__description">20-04-2024</p>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className="card__article">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_3.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
+        <SwiperSlide className='card__article '>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_3.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
             </div>
 
-            <div className="card__data">
-              <h3 className="card__name">Registration Ends</h3>
-              <p className="card__description">27-04-2024</p>
-            </div>
-          </SwiperSlide>
+                <div className="card__data">
+                        <h3 className="card__name">Project Submission</h3>
+                        <p className="card__description">
+                          29-04-2024
+                        </p>
+                 </div>
 
-          <SwiperSlide className="card__article ">
-            <div className="card__image">
-              <img
-                src={require("../../assets/img_3.png")}
-                className="card__img"
-              />
-              <div className="card__shadow"></div>
+                
+            
+        </SwiperSlide>
+
+        <SwiperSlide className='card__article'>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_2.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
             </div>
 
-            <div className="card__data">
-              <h3 className="card__name">Project Submission</h3>
-              <p className="card__description">29-04-2024</p>
+                <div className="card__data">
+                        <h3 className="card__name">Registration Begins</h3>
+                        <p className="card__description">
+                           20-04-2024
+                        </p>
+                 </div>
+       </SwiperSlide>
+
+       <SwiperSlide className='card__article'>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_3.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
             </div>
-          </SwiperSlide>
+
+                <div className="card__data">
+                        <h3 className="card__name">Registration Ends</h3>
+                        <p className="card__description">
+                           27-04-2024
+                        </p>
+                 </div>
+
+                
+            
+        </SwiperSlide>
+
+        <SwiperSlide className='card__article '>
+        
+            <div className='card__image'>
+                  <img src={require('./assets/img_3.png')} className='card__img'/>
+                  <div className="card__shadow"></div>
+                
+            </div>
+
+                <div className="card__data">
+                        <h3 className="card__name">Project Submission</h3>
+                        <p className="card__description">
+                          29-04-2024
+                        </p>
+                 </div>
+
+                
+            
+        </SwiperSlide>
+
+   
         </div>
 
         <div className="slider-controler">
@@ -139,9 +183,27 @@ const Timeline = () => {
           </div>
           <div className="swiper-pagination"></div>
         </div>
-      </Swiper>
-    </div>
-  );
-};
 
-export default Timeline;
+
+      </Swiper>
+      </div>
+
+
+      <div className="swiper-button-next">
+               <i className="ri-arrow-right-s-line"></i>
+       </div>
+            
+            <div className="swiper-button-prev">
+               <i className="ri-arrow-left-s-line"></i>
+            </div>
+
+           
+            {/* <div className="swiper-pagination"></div> */}
+        
+
+     
+   </div>
+  )
+}
+
+export default Timeline
