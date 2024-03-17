51b670e4b01eab2421ba2fe15f801936547dbb4b
AlaynaShaheen <111679498+AlaynaShaheen@users.noreply.github.com>
2024-03-17 19:11:28 +0530

    prizes part (#13)

---
+++ src/Prizes.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import './Prizes.css';
+const Prizes = () => {
+  return (
+    <div className='page'>
+      
+      
+      <div className='prizes'>
+        <p className='heading'>PRIZES</p>
+      </div>
+      <div className='combined'>
+      <div className='left'>
+      <svg width="300" height="430" xmlns="http://www.w3.org/2000/svg">
+  <rect width="200" height="500" x="10" y="10"  fill="grey" />
+</svg>
+<svg className='one' width="148px" height="148px" viewBox="-2.4 -2.4 28.80 28.80" xmlns="http://www.w3.org/2000/svg">
+  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="white" stroke-width="0.144"></g>
+  <g id="SVGRepo_iconCarrier">
+    <g clip-path="url(#clip0_429_11095)">
+   
+      <circle cx="12" cy="12" r="9" stroke="black" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"></circle>
+
+      <circle cx="12" cy="12" r="8" stroke="#FFEA00" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="none"></circle>
+   
+      <circle cx="12" cy="12" r="6" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="white"></circle>
+  
+      <text x="12" y="16" fill="black" font-size="10px" text-anchor="middle">1</text> {/* Number "1" inside the circle */}
+    </g>
+    <defs>
+      <clipPath id="clip0_429_11095">
+        <rect width="24" height="24" fill="white"></rect>
+      </clipPath>
+    </defs>
+  </g>
+</svg>
+
+
+<svg className='two' width="148px" height="148px" viewBox="-2.4 -2.4 28.80 28.80" xmlns="http://www.w3.org/2000/svg">
+  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="white" stroke-width="0.144"></g>
+  <g id="SVGRepo_iconCarrier">
+    <g clip-path="url(#clip0_429_11095)">
+  
+      <circle cx="12" cy="12" r="9" stroke="black" stroke-width="0.6" stroke-linecap="round" stroke-linejoin="round"></circle>
+
+      <circle cx="12" cy="12" r="8" stroke="#0F52BA" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" fill="none"></circle>
+
+      <circle cx="12" cy="12" r="6" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" fill="white"></circle>
+
+      <text x="12" y="16" fill="black" font-size="10px" text-anchor="middle">2</text> {/* Number "1" inside the circle */}
+    </g>
+    <defs>
+      <clipPath id="clip0_429_11095">
+        <rect width="24" height="24" fill="white"></rect>
+      </clipPath>
+    </defs>
+  </g>
+</svg>
+<svg className='three' width="148px" height="148px" viewBox="-2.4 -2.4 28.80 28.80" xmlns="http://www.w3.org/2000/svg">
+  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="white" stroke-width="0.144"></g>
+  <g id="SVGRepo_iconCarrier">
+    <g clip-path="url(#clip0_429_11095)">
+
+      <circle cx="12" cy="12" r="9" stroke="black" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"></circle>
+
+      <circle cx="12" cy="12" r="8" stroke="#00FFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="none"></circle>
+ 
+      <circle cx="12" cy="12" r="6" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" fill="white"></circle>
+
+      <text x="12" y="16" fill="black" font-size="10px" text-anchor="middle">3</text> {/* Number "1" inside the circle */}
+    </g>
+    <defs>
+      <clipPath id="clip0_429_11095">
+        <rect width="24" height="24" fill="white"></rect>
+      </clipPath>
+    </defs>
+  </g>
+</svg>
+
+</div>
+<div className='left right'>
+<svg width="300" height="430" xmlns="http://www.w3.org/2000/svg">
+  <rect width="300" height="500" x="10" y="10" fill="grey" />
+</svg>
+<div className='opace f'>
+      <p className='first'>15000</p>
+      
+      </div>
+      <div class="p1 container"> 
+        <div class="skill html yellow">'</div> 
+    </div> 
+      <div className='opace s'>
+      <p className='second'>10000</p>
+      </div>
+      <div class="p2 container"> 
+        <div class="skill html blue">'</div> 
+    </div> 
+      <div className='opace t'>
+      <p className='third'>5000</p>
+      </div>
+      <div class="p3 container"> 
+        <div class="skill html cyan">'</div> 
+    </div> 
+      </div>
+</div>
+        
+    </div>
+  )
+}
+
+export default Prizes
