17cf1e4d1ba593937ec27128b5894483b46c54dc
uday1508 <udayg256@gmail.com>
2024-03-17 16:42:15 +0530

    pull-mrge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import "./App.css";
 import Timeline from "./Timelines/Timeline";
+import Navbar from "./components/nav.jsx";
 
 function App() {
 
   return (
   <div className="App">
+     < Navbar/>
   <Timeline/>
     </div>
   )
-import Navbar from "./components/nav.jsx";
-function App() {
-  return (
-  <div className="App">
-    < Navbar/>
-    </div>
-  )
-
-  return <div className="App">Testing sus hackkkkkkkkkkkkk</div>;
-
-
-}
+  }
 
 export default App;
