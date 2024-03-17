8ceaf655c4c48ce6487e639a90ce39cc5d3edd24
Giribabi <108036188+Giribabi@users.noreply.github.com>
2024-03-17 19:32:08 +0530

    Created About Component (#15)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "./App.css";
+<<<<<<< HEAD
+import About from "./Components/About/About";
+
+function App() {
+    return (
+        <div className="App">
+            Testing sus hackkkkkkkkkkkkk
+            <About />
+        </div>
+    );
+=======
 import Sponsors from "./components/Sponsors/Sponsors.js";
 import Contact from "./components/Contact Us/Contactus.js";
 import Prizes from "./components/Prizes/Prizes.js";
@@ -19,6 +30,7 @@ function App() {
       <Contact />
     </div>
   );
+>>>>>>> 51b670e4b01eab2421ba2fe15f801936547dbb4b
 }
 
 export default App;
