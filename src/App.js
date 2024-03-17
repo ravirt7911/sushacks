f09622709685ff85f7b3544b93195a76155d3c3a
Rokkam Lakshmi Amrutha <amrutharokkam573@gmail.com>
2024-03-17 17:46:45 +0530

    Sponsers (#12)
    * sponsers
    
    * sponsers
    
    * sponsers
    
    * sponsers
    
    ---------
    
    Co-authored-by: Kamsu Ravi Teeja <94612127+ravirt7911@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import Sponsers from "./components/sponsers/sponsers.jsx";
+
 import Tracks from "./components/Tracks/Tracks";
+
 import Navbar from "./components/Navbar/Navbar";
 import Timeline from "./components/Timelines/Timeline";
+
 
 function App() {
   return (
@@ -10,6 +14,7 @@ function App() {
       <Navbar />
       <Tracks />
       <Timeline />
+      <Sponsers/>
     </div>
   );
 }
