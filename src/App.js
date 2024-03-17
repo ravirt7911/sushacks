4bbb77909cc601441be2e140c395ddaf2b9b73f4
Giribabi <sammidisaroja@gmail.com>
2024-03-17 20:44:40 +0530

    Updated all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import About from "./Components/About/About";
 import Sponsors from "./Components/Sponsors/Sponsors.js";
 import Contact from "./Components/Contact Us/Contactus.js";
@@ -21,27 +20,6 @@ function App() {
             <Contact />
         </div>
     );
-=======
-import Sponsors from "./components/Sponsors/Sponsors.js";
-import Contact from "./components/Contact Us/Contactus.js";
-import Prizes from "./components/Prizes/Prizes.js";
-import Tracks from "./components/Tracks/Tracks.js";
-
-import Navbar from "./components/Navbar/Navbar.js";
-// import Timeline from "./components/Timelines/Timeline.js";
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Tracks />
-      {/* <Timeline /> */}
-      <Prizes />
-      <Sponsors />
-      <Contact />
-    </div>
-  );
->>>>>>> 8a3272b3a1711c0f729184821d745555d181cf8a
 }
 
 export default App;
