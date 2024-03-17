7deb92f0d8ce0cceff1c702a4745b6858bb37d4e
ravirt7911 <ravirt7911@gmail.com>
2024-03-17 19:26:37 +0530

    Prizes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-
-
 import React from "react";
 import "./App.css";
 import Sponsors from "./components/Sponsors/Sponsors.js";
 import Contact from "./components/Contact Us/Contactus.js";
+import Prizes from "./components/Prizes/Prizes.js";
+import Tracks from "./components/Tracks/Tracks.js";
 
-import Tracks from "./components/Tracks/Tracks";
-
-import Navbar from "./components/Navbar/Navbar";
-import Timeline from "./components/Timelines/Timeline";
+import Navbar from "./components/Navbar/Navbar.js";
+// import Timeline from "./components/Timelines/Timeline.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Tracks />
-      <Timeline />
+      {/* <Timeline /> */}
+      <Prizes />
       <Sponsors />
       <Contact />
     </div>
