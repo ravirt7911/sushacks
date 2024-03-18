dcd31a5ead992c8992579a3722483e84651d9042
ravirt7911 <ravirt7911@gmail.com>
2024-03-18 20:40:55 +0530

    ok

--- src/components/FAQs/AccordionItem.js
+++ src/components/FAQs/AccordionItem.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import "./Faqs.css";
 
-const AccordionItem = ({ question, answer, containerBackgroundImage }) => {
-  const [showAnswer, setShowAnswer] = useState(false);
+const AccordionItem = ({ question, answer, backgroundImage }) => {
+  const [expanded, setExpanded] = useState(false);
 
-  const toggleAnswer = () => {
-    setShowAnswer(!showAnswer);
+  const toggleAccordion = () => {
+    setExpanded(!expanded);
   };
 
   return (
     <div
-      className="accordion-container"
-      style={{ backgroundImage: `url(${containerBackgroundImage})` }}
+      className={`accordion-container ${expanded ? "expanded" : ""}`}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="accordion-item">
         <div className="question">{question}</div>
-        <button className="toggle-button" onClick={toggleAnswer}>
-          {showAnswer ? "▲" : "▼"}
+        <button className="toggle-button" onClick={toggleAccordion}>
+          {expanded ? "▲" : "▼"}
         </button>
-        {showAnswer && <div className="answer">{answer}</div>}
       </div>
+      <div className="answer">{answer}</div>
     </div>
   );
 };
--- src/components/FAQs/Faqs.js
+++ src/components/FAQs/Faqs.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Faqs.css";
 import AccordionItem from "./AccordionItem.js";
 import Pic from "../../assets/among 1 (1).svg";
-import Pic1 from "../../assets/Group 377.svg";
+import Pic1 from "../../assets/Rectangle 152.svg";
 
 const Faqs = () => {
   return (
-    <div className="faqs">
+    <div className="faqs" style={{ backgroundImage: { Pic1 } }}>
       <div className="content">
         <img src={Pic} alt="Among Us" className="image" />
         <h1>FAQS</h1>
@@ -14,15 +14,9 @@ const Faqs = () => {
       <div className="questions">
         <AccordionItem
           question="When and where will Sus-Hacks 2024 takes place? "
-          answer="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-          backgroundImage={Pic1}
+          answer="Lorem Ipsum is simply dummy text of the printing and ."
         />
-        <AccordionItem
-          question="Question 2"
-          answer="Answer 2"
-          backgroundImage={Pic1}
-        />
-        {/* Add more AccordionItem components as needed */}
+        <AccordionItem question="Question 2" answer="Answer 2" />
       </div>
     </div>
   );
