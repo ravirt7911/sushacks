33df808cb5edadb3a315e63b2ac478dd3fbd9685
uday1508 <udayg256@gmail.com>
2024-03-18 14:15:34 +0530

    %75%

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import React from "react";
 import "./App.css";
 import Sponsors from "./components/Sponsors/Sponsors.js";
 import Contact from "./components/Contact Us/Contactus.js";
-
-import Tracks from "./components/Tracks/Tracks";
-
-import Navbar from "./components/Navbar/Navbar";
-import Timeline from "./components/Timelines/Timeline";
+import Prizes from "./components/Prizes/Prizes.js";
+import Tracks from "./components/Tracks/Tracks.js";
+import Home from "./components/Home/Home.jsx";
+import Navbar from "./components/Navbar/Navbar.js";
+import About from "./components/About/About.jsx";
+import Faqs from "./components/FAQs/Faqs.js";
+// import Timeline from "./components/Timelines/Timeline.js";
 
 function App() {
   return (
